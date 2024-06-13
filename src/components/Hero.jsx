3ce75a02ce6097332bf907352a105ecf75313ad1
deep_patel <102026937+deeplegend@users.noreply.github.com>
2024-06-13 15:20:50 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion"
 import { styles } from '../styles'
+import { EarthCanvas } from "./canvas"
 import { slideIn} from '../utils/motion'
 
 const Hero = () => {
@@ -20,6 +21,9 @@ const Hero = () => {
             <a href="" className='  text-black '>Get Started Today</a>
           </div>
         </div>
+
+        <EarthCanvas />
+      
       </div>
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
